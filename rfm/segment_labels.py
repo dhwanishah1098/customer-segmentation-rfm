c796833f98f24287e8c82708c44a6b615de0751b
Dhwani Shah <dhwanishah1098@gmail.com>
2024-09-08 10:00:00 +1000

    Add RFM segment label mapping

---
+++ rfm/segment_labels.py
@@ -0,0 +1,8 @@
+SEGMENT_LABELS = {
+    'champions': 'Champions',
+    'loyal': 'Loyal Customers',
+    'at_risk': 'At Risk',
+    'hibernating': 'Hibernating',
+    'lost': 'Lost',
+    'promising': 'Promising',
+}
