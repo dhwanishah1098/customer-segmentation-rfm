54c796a2e5643bb646618acd861c9896ace5cc48
Dhwani Shah <dhwanishah1098@gmail.com>
2025-01-10 13:00:00 +1100

    Add low frequency customer filter

--- rfm/utils.py
+++ rfm/utils.py
@@ -1,3 +1,6 @@
 
 def high_value_customers(rfm, threshold=1000):
     return rfm[rfm["monetary"] >= threshold]
+
+def low_frequency_customers(rfm, max_f=2):
+    return rfm[rfm["frequency"] <= max_f]
