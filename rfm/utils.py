a7899c2f62f7624b9ab5946701c282b8d7835013
Dhwani Shah <dhwanishah1098@gmail.com>
2025-08-13 15:00:00 +1000

    Add at risk customer counter

--- rfm/utils.py
+++ rfm/utils.py
@@ -67,3 +67,6 @@ def high_value_customers(rfm, threshold=1000):
 
 def low_frequency_customers(rfm, max_f=2):
     return rfm[rfm["frequency"] <= max_f]
+
+def at_risk_count(rfm):
+    return (rfm["segment"] == "At Risk").sum()
