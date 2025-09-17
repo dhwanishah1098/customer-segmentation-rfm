d51be6021cd56c8962719ac3a9df814c9489d031
Dhwani Shah <dhwanishah1098@gmail.com>
2025-09-17 15:00:00 +1000

    Add segment count summary

--- rfm/utils.py
+++ rfm/utils.py
@@ -76,3 +76,6 @@ def champions_revenue(rfm):
 
 def rfm_stats(rfm):
     return rfm[["recency","frequency","monetary"]].describe().round(2)
+
+def segment_counts(rfm):
+    return rfm["segment"].value_counts().to_dict()
